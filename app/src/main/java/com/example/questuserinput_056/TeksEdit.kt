b48d8c17b8e4f77cba5dbdd5e1e9c4812e7c41b7
Dababah <pawasalee96@gamil.com>
2025-10-30 12:08:29 +0700

    tambahkan composable FormDataDiri dengan state untuk input data diri dan daftar gender

--- app/src/main/java/com/example/questuserinput_056/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_056/TeksEdit.kt
@@ -1,5 +1,20 @@
 package com.example.questuserinput_056
 
+@Composable
+fun FormDataDiri(modifier: Modifier
+){
+    // variabel-variabel untuk mengingat nilai masukan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf(" ") }
+    var textJK by remember { mutableStateOf("") }
+
+    // variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf(" ") }
+    var jenis by remember { mutableStateOf("") }
+
+    val gender:List<String> = listOf("Laki-laki","Perempuan")
+
 Row {
     gender.forEach { item ->
         Row (modifier = Modifier.selectable(
