cd19cdf9f1e64faf7e0e8e44713f600f7c78796d
Dababah <pawasalee96@gamil.com>
2025-10-30 12:29:11 +0700

    perbaiki sintaks ElevatedCard dengan elevation, warna latar, dan ukuran tetap

--- app/src/main/java/com/example/questuserinput_056/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_056/TeksEdit.kt
@@ -1,8 +1,14 @@
 package com.example.questuserinput_056
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
@@ -86,3 +92,12 @@ onValueChange = {
         )),
             thickness = dimensionResource(R.dimen.divider_tipis),
             color = color.darkgray)
+
+            ElevatedCard {
+                elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
+                colors = CardDefaults.cardColors(containerColor = Color.Black),
+                modifier = Modifier
+                    .height(100.dp)
+                    .width(300.dp)
+            }
+
